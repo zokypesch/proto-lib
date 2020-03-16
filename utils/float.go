ed6a1749491b4d2a03c20d140b8499e110f986e7
zokypesch <maulanaoktofitriadi@gmail.com>
2020-03-16 15:30:25 +0700

    added utils float

---
+++ utils/float.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"encoding/binary"
+	"math"
+)
+
+// Float64frombytes for convert byte to float64
+func Float64frombytes(bytes []byte) float64 {
+	bits := binary.LittleEndian.Uint64(bytes)
+	float := math.Float64frombits(bits)
+	return float
+}
+
+// Float64bytes for convert float64 to byte
+func Float64bytes(float float64) []byte {
+	bits := math.Float64bits(float)
+	bytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(bytes, bits)
+	return bytes
+}
