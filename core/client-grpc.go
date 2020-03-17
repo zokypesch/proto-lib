787ddbe9f4d619cdce3b48f22e3d468e7297a808
zokypesch <maulanaoktofitriadi@gmail.com>
2020-03-17 13:21:01 +0700

    added

--- core/client-grpc.go
+++ core/client-grpc.go
@@ -4,16 +4,21 @@ import (
 	"context"
 	"time"
 
+	"github.com/zokypesch/proto-lib/utils"
 	"google.golang.org/grpc/metadata"
 )
 
 // CreateContextForOutgoing for create context for outgoing request
-func CreateContextForOutgoing(req map[string]string) context.Context {
+func CreateContextForOutgoing(ctxIncome context.Context, req map[string]string) context.Context {
 	md := metadata.MD{}
 	for k, v := range req {
 		md[k] = []string{v}
 	}
+	auth, ok := utils.GetTokenFromCtx(ctxIncome)
 
+	if ok {
+		md["authorization"] = []string{auth}
+	}
 	ctx := newCtx(5 * time.Second)
 
 	newCtx := metadata.NewIncomingContext(ctx, md)
