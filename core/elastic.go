c8430d2d8b275e957069a0f4315226662b376d7f
zokypesch <maulanaoktofitriadi@gmail.com>
2020-04-13 09:50:28 +0700

    added bulk delete

--- core/elastic.go
+++ core/elastic.go
@@ -109,7 +109,7 @@ func (es *ESCore) BulkAddDocument(ctx context.Context, IDs []string, body []inte
 		req := elastic.NewBulkIndexRequest().Index(es.indexName).Type(es.typeIndex).Id(ID).Doc(v)
 		bulkRequest = bulkRequest.Add(req)
 	}
-	_, err := bulkRequest.Do()
+	_, err := bulkRequest.Do(ctx)
 
 	return err
 }
