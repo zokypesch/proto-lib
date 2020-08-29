659540a6343ff499135116d72a4df3b58ba0fead
zokypesch <maulanaoktofitriadi@gmail.com>
2020-08-30 02:10:38 +0700

    added health check

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # proto-lib
 
-git tag -a v3.1.0 -m "Add loging with apm errors"
+git tag -a v3.1.1 -m "Add health check"
 
--- core/http.go
+++ core/http.go
@@ -31,7 +31,9 @@ func RunHTTP(init func() error, registerHandler func(ctx context.Context, mux *r
 }
 
 // RunHTTPWithCustomMatcher for running http
-func RunHTTPWithCustomMatcher(init func() error, registerHandler func(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error), customMatcher func(key string) (string, bool), GRPCAddress string, GRPCPort string, HTTPPort string) error {
+func RunHTTPWithCustomMatcher(init func() error, registerHandler func(ctx context.Context, mux *runtime.ServeMux,
+	endpoint string, opts []grpc.DialOption) (err error), customMatcher func(key string) (string, bool),
+	GRPCAddress string, GRPCPort string, HTTPPort string, patt runtime.Pattern) error {
 	if err := init(); err != nil {
 		return err
 	}
@@ -41,6 +43,11 @@ func RunHTTPWithCustomMatcher(init func() error, registerHandler func(ctx contex
 	defer cancel()
 
 	mux := runtime.NewServeMux(runtime.WithIncomingHeaderMatcher(customMatcher))
+
+	mux.Handle("GET", patt, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		w.WriteHeader(200)
+	})
+
 	opts := []grpc.DialOption{grpc.WithInsecure()}
 	err := registerHandler(ctx, mux, fmt.Sprintf("%s:%s", GRPCAddress, GRPCPort), opts)
 
