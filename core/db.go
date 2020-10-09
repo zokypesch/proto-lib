a9438cd6249d855c9b67d0112ba5dc2c24d3fc4e
zokypesch <maulanaoktofitriadi@gmail.com>
2020-10-09 13:53:25 +0700

    add without max

--- core/db.go
+++ core/db.go
@@ -30,6 +30,25 @@ func InitDB(address string, dbName string, user string, password string, DBPort 
 	return db
 }
 
+// InitDBWithoutMax for initial database
+func InitDBWithoutMax(address string, dbName string, user string, password string, DBPort int, logMode bool) *gorm.DB {
+	dbUser := user
+	dbPass := password
+	dbEndpoint := address
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbEndpoint, DBPort, dbName)
+
+	db, err := gorm.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf(err.Error())
+		panic(err)
+	}
+
+	db.LogMode(logMode)
+	db.DB().SetConnMaxLifetime(time.Minute * time.Duration(10))
+	db.SingularTable(true)
+	return db
+}
+
 // InitDBWithoutLog for initial database
 func InitDBWithoutLog(address string, dbName string, user string, password string, DBPort int) *gorm.DB {
 	dbUser := user
