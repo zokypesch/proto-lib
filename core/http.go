062276b649fb646e28276a679d3fc6bb35bf720d
harry.sitohang <harry.sitohang@prakerja.go.id>
2022-02-09 14:57:45 +0700

    add route api/v1/%s/health

--- core/http.go
+++ core/http.go
@@ -9,8 +9,10 @@ import (
 	"google.golang.org/grpc"
 )
 
-// RunHTTP for running http
-func RunHTTP(init func() error, registerHandler func(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error), GRPCAddress string, GRPCPort string, HTTPPort string) error {
+// RunHTTPWithCustomMatcher for running http
+func RunHTTPWithCustomMatcherV2(init func() error, registerHandler func(ctx context.Context, mux *runtime.ServeMux,
+	endpoint string, opts []grpc.DialOption) (err error), customMatcher func(key string) (string, bool),
+	GRPCAddress string, GRPCPort string, HTTPPort string, patt runtime.Pattern, svcUriPath string) error {
 	if err := init(); err != nil {
 		return err
 	}
@@ -19,7 +21,17 @@ func RunHTTP(init func() error, registerHandler func(ctx context.Context, mux *r
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	mux := runtime.NewServeMux()
+	mux := runtime.NewServeMux(runtime.WithIncomingHeaderMatcher(customMatcher))
+
+	mux.Handle("GET", patt, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		w.WriteHeader(200)
+	})
+
+	patternV1Health := runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", svcUriPath, "health"}, "", nil...))
+	mux.Handle("GET", patternV1Health, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		w.WriteHeader(200)
+	})
+
 	opts := []grpc.DialOption{grpc.WithInsecure()}
 	err := registerHandler(ctx, mux, fmt.Sprintf("%s:%s", GRPCAddress, GRPCPort), opts)
 
@@ -48,6 +60,27 @@ func RunHTTPWithCustomMatcher(init func() error, registerHandler func(ctx contex
 		w.WriteHeader(200)
 	})
 
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := registerHandler(ctx, mux, fmt.Sprintf("%s:%s", GRPCAddress, GRPCPort), opts)
+
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(fmt.Sprintf(":%s", HTTPPort), mux)
+}
+
+// RunHTTP for running http
+func RunHTTP(init func() error, registerHandler func(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error), GRPCAddress string, GRPCPort string, HTTPPort string) error {
+	if err := init(); err != nil {
+		return err
+	}
+	runtime.HTTPError = CustomHTTPError
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithInsecure()}
 	err := registerHandler(ctx, mux, fmt.Sprintf("%s:%s", GRPCAddress, GRPCPort), opts)
 
---
+++ go.mod
@@ -0,0 +1,34 @@
+module github.com/zokypesch/proto-lib
+
+go 1.15
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/elastic/go-sysinfo v1.4.0 // indirect
+	github.com/elastic/go-windows v1.0.1 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/golang/protobuf v1.3.4
+	github.com/gomodule/redigo v2.0.0+incompatible
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.0
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
+	github.com/grpc-ecosystem/grpc-gateway v1.14.3
+	github.com/jinzhu/gorm v1.9.12
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/mailru/easyjson v0.7.1 // indirect
+	github.com/oklog/ulid v1.3.1
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.1.0
+	github.com/prometheus/common v0.9.1 // indirect
+	github.com/prometheus/procfs v0.0.11 // indirect
+	github.com/sirupsen/logrus v1.4.2
+	github.com/sony/sonyflake v1.0.0
+	go.elastic.co/apm v1.15.0
+	go.elastic.co/apm/module/apmgrpc v1.15.0
+	go.elastic.co/apm/module/apmlogrus v1.15.0
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
+	google.golang.org/genproto v0.0.0-20200312145019-da6875a35672 // indirect
+	google.golang.org/grpc v1.28.0
+	gopkg.in/olivere/elastic.v5 v5.0.84
+	howett.net/plist v0.0.0-20200419221736-3b63eb3a43b5 // indirect
+)
